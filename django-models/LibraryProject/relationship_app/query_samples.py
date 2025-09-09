3ab39a3455761e1937a5ebc51aee7497f0a2ebfc
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-09-09 16:12:00 +0100

    retrieve librarian books code structure added

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -14,12 +14,21 @@ def get_library_by_name(library_name):
     return Library.objects.get(name=library_name)
 
 
-
+# retrieve books by author name
 from .models import Author, Book
 
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
+
+
+# retrieve the librarian for a given library.
+from .models import Author, Book, Library, Librarian
+
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
+
 
 
 def run_queries():

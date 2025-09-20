3297673a3d4023f590a892463e882c8d86e89684
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-09-20 23:12:47 +0100

    codes for viewing books added to views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    books = Book.objects.all()   # <-- required query
+    return render(request, "relationship_app/list_books.html", {"books": books})  # <-- required template

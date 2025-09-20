2508b98756a2651c20e8f7bc1c47c202bb6f5959
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-09-20 23:22:19 +0100

    library view function added to views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,3 +7,13 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()   # <-- required query
     return render(request, "relationship_app/list_books.html", {"books": books})  # <-- required template
+
+
+# others
+from django.shortcuts import render, get_object_or_404
+from .models import Library
+
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
+

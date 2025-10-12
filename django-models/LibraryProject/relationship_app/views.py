fafa76d2e1fea971b0152e649db4820faa3c7465
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-12 21:16:21 +0100

    relationship_app/views.py codes for user role-base control.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -69,4 +69,34 @@ def logout_view(request):
 # -------------------------------
 @login_required
 def home_view(request):
-    return render(request, 'relationship_app/home.html', {'user': request.user})
+    return render(request, 'relationship_app/home.html', {'user': request.user}) 
+
+# -----------------------------------
+# Role check functions 
+# ----------------------------------- 
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# ---------------------------
+# Views 
+# --------------------------
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')

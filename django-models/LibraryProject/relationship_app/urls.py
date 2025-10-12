f3f953697e7315003a62c82b4347f3429b95f6c7
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-12 22:20:43 +0100

    urls.py, codes for  user role path added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -81,5 +81,13 @@ urlpatterns = [
     # Optional Homepage
     # -------------------------------
     path("", views.home_view, name="home"),
+
+    # --------------------------------
+    # user-role views 
+    # --------------------------------
+    path('admin/dashboard/', views.admin_view, name='admin_view'),
+    path('librarian/dashboard/', views.librarian_view, name='librarian_view'),
+    path('member/dashboard/', views.member_view, name='member_view'),
 ]
+
 

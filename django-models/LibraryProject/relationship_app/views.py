6620643ad2273c2f92ed404e85231aae060cdb20
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-13 06:13:05 +0100

    code corrections on relationship_app/views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,15 @@
 
 # Create your views here.
 from django.shortcuts import render
-from .models import Book
+from .models import Book 
+from django.shortcuts import render, get_object_or_404
+from .models import Library 
+from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     books = Book.objects.all()   # <-- required query
@@ -10,21 +18,11 @@ def list_books(request):
 
 
 # others
-from django.shortcuts import render, get_object_or_404
-from .models import Library 
-
-from django.views.generic.detail import DetailView
-
 def library_detail(request, library_id):
     library = get_object_or_404(Library, id=library_id)
     return render(request, 'relationship_app/library_detail.html', {'library': library})
 
 # relationship_app/views.py
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required
-
 # -------------------------------
 # Register View
 # -------------------------------
@@ -38,7 +36,6 @@ def register_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
-
 
 # -------------------------------
 # Login View
@@ -83,20 +80,45 @@ def is_librarian(user):
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
+
+# --------------------------------
+# add checks before your views
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin' 
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian' 
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member' 
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+
 # ---------------------------
 # Views 
 # --------------------------
-@login_required
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_view.html')
-
-@login_required
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
-
-@login_required
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
+# @login_required
+# @user_passes_test(is_admin)
+# def admin_view(request):
+#     return render(request, 'admin_view.html')
+# 
+# @login_required
+# @user_passes_test(is_librarian)
+# def librarian_view(request):
+#     return render(request, 'librarian_view.html')
+# 
+# @login_required
+# @user_passes_test(is_member)
+# def member_view(request):
+#     return render(request, 'member_view.html')

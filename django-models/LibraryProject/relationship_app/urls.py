cb9b7a23562a918d333609212280625e10d8ce28
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-13 05:44:43 +0100

    codes for role-based views changed. relationship_app/views.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -85,9 +85,14 @@ urlpatterns = [
     # --------------------------------
     # user-role views 
     # --------------------------------
-    path('admin/dashboard/', views.admin_view, name='admin_view'),
-    path('librarian/dashboard/', views.librarian_view, name='librarian_view'),
-    path('member/dashboard/', views.member_view, name='member_view'),
+    # path('admin/dashboard/', views.admin_view, name='admin_view'),
+    # path('librarian/dashboard/', views.librarian_view, name='librarian_view'),
+    # path('member/dashboard/', views.member_view, name='member_view'),
+
+    # Role-based access views
+    path('admin-view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', views.librarian_view, name='librarian_view'),
+    path('member-view/', views.member_view, name='member_view'),
 ]
 
 

5afea2f6305dd4a4ae964d4ddf801c336ce0f51b
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-18 21:16:18 +0100

    codes added to admin.py file, to extend useradmin class

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 
 # Register your models here.
+
+
+# -------------------------
+# Custom User Admin
+# -------------------------
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "first_name", "last_name", "date_of_birth", "is_staff")
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+

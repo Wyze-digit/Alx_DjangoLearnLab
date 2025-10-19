cb192396d27ec37050ef58aea6bb488e996fadb6
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-19 14:50:02 +0100

    bookshelf.views.py file .. code implementation to enforce permission-based access to CRUD actions for the Book model.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.contrib import messages
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        published_date = request.POST.get('published_date')
+        isbn = request.POST.get('isbn')
+        description = request.POST.get('description')
+
+        Book.objects.create(
+            title=title,
+            author=author,
+            published_date=published_date,
+            isbn=isbn,
+            description=description
+        )
+        messages.success(request, "Book added successfully.")
+        return redirect('book_list')
+
+    return render(request, 'bookshelf/add_book.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = request.POST.get('author')
+        book.published_date = request.POST.get('published_date')
+        book.isbn = request.POST.get('isbn')
+        book.description = request.POST.get('description')
+        book.save()
+        messages.success(request, "Book updated successfully.")
+        return redirect('book_list')
+
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        messages.success(request, "Book deleted successfully.")
+        return redirect('book_list')
+
+    return render(request, 'bookshelf/delete_book.html', {'book': book})

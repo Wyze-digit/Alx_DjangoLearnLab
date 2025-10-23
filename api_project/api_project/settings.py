c1a393c57dea5a53636686ecc46717e2a59b3512
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-23 11:26:57 +0100

    corrections made to file models.py, urls.py file

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF Configuration (optional starter config)
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}

14e1da80045b9fd9e693a53b067f9e63c7775376
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-24 06:48:21 +0100

    api/views.py serializers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,10 +5,22 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 
 
 class BookList(generics.ListAPIView):
     """API view to list all books in the database."""
     
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# """
+# A ViewSet for viewing and editing Book instances.
+# Supports:
+# - GET (list and retrieve)
+# - POST (create)
+# - PUT/PATCH (update)
+# - DELETE (destroy)
+# """
+queryset = Book.objects.all()
     serializer_class = BookSerializer

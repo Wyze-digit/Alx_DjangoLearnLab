37edcf168de2f9d41ea08f38666f9ee0a305e91d
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-24 07:02:37 +0100

    bookviewset code added views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -14,6 +14,8 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+class BookViewSet(viewsets.ModelViewSet):
+
 # """
 # A ViewSet for viewing and editing Book instances.
 # Supports:

e500fb867880bf190a808e5032bb6a119c8bdae0
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-24 06:55:45 +0100

    urlpatterns, routers added api/urls.py file

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,18 @@
 # api/urls.py
 
 from django.urls import path
-from .views import BookList
+from .views import BookList 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from .views import BookViewSet
+from .views import BookViewSet
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'), 
+    path('', include(router.urls)),  # Includes all auto-generated CRUD routes
 ]
+
+# Create a router and register our ViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')

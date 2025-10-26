702721f1d502a8c67bfee820ddb67f4f8cfddfc0
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-26 08:36:42 +0100

    correction in models.py, views.py, serializers.py for item serializations and views done

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -8,3 +8,12 @@ class Book(models.Model):
         return self.title
 
 # Create your models here.
+# api/models.py
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,11 +2,17 @@
 
 from rest_framework import serializers
 from .models import Book
-
+from .models import Item
 
 class BookSerializer(serializers.ModelSerializer):
     """Serializer to convert Book model instances into JSON format."""
     
     class Meta:
         model = Book
-        fields = '__all__'  # Include all fields of the Book model
+        fields = '__all__'  # Include all fields of the Book model  
+
+# api/serializers.py
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'

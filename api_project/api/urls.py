628faef0378bb886c66d1fca27e7d6734d5f081b
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-10-26 10:12:09 +0100

    corrections done, tested model running. save all codes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,5 @@
 # api/urls.py
-
-from django.urls import path
+from . import views
 from .views import BookList 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -8,11 +7,19 @@ from .views import BookViewSet
 from .views import BookViewSet
 from .views import BookViewSet
 
-urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'), 
-    path('', include(router.urls)),  # Includes all auto-generated CRUD routes
-]
+
+
 
 # Create a router and register our ViewSet
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
+router.register(r'items', views.ItemViewSet, basename='item') # itemviewset registration
+router.register(r'admin-items', views.AdminItemViewSet, basename='admin-item')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('public/', views.public_info, name='public-info'),
+    path('books/', BookList.as_view(), name='book-list'), 
+]
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets
 from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response

fc92f1d0278e7b976329888ba1365c476d7d6faf
Wisdom Emmanuel <ifiokbassey.e@gmail.com>
2025-11-02 16:10:47 +0100

    change database to sqllite

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+"""
 # PostgreSQL Database Configuration
 DATABASES = {
     'default': {
@@ -26,6 +27,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -36,7 +38,7 @@ SECRET_KEY = 'django-insecure-wa2)(ic*i3yaji+x_&)!x+b)muv@2%j3cl2hu%ia((o8wrfo%1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','*.*']
 
 
 # Application definition
@@ -92,14 +94,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
